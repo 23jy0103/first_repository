2b51982f36ab10e4de2a02d65fb219be284f517a
磯部 <23jy0103@jec.ac.jp>
2024-10-16 15:29:51 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args){
-    System.out.println("Hello World!,Hello GitHub!!");
+    System.out.println("HelloWorld");
+    System.out.println("HelloWorld");
+    System.out.println("日本電子専門学校");
   }
 }
