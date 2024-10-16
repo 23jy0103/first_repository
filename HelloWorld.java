8c72020dff9bfd9ec76b5afe24fa722d74e7db77
磯部 <23jy0103@jec.ac.jp>
2024-10-16 16:28:30 +0900

    HelloWorld.java
    スマートフォンで入力した内容と顔文字の追加をしました。

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ public class HelloWorld {
     System.out.println("HelloWorld");
     System.out.println("HelloWorld");
     System.out.println("日本電子専門学校");
+    System.out.println("スマートフォンで設定しています！！( 厂˙ω˙ )厂うぇーい");
+    System.out.println("( ᐛ )( ܸ ⩌⩊⩌ ܸ )");
   }
+
 }
