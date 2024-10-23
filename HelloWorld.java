a4d9b5e60a427777f4e480ef33ab230a67a4e9b4
磯部 <23jy0103@jec.ac.jp>
2024-10-23 17:45:45 +0900

    HelloWorld.java
    10行目 Scanner sc = new Scanner(System.in);に変更しました

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
     System.out.println("日本電子専門学校");
     System.out.println("スマートフォンで設定しています！！( 厂˙ω˙ )厂うぇーい");
     System.out.println("( ᐛ )( ܸ ⩌⩊⩌ ܸ )");
-    Scanner sc = new Scanner(system.in);
+    Scanner sc = new Scanner(System.in);
     String hello = sc.next();
     System.out.println(hello);
   }
